b42aecda73429a8229a95261072cfff347e9017a
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-12-24 15:46:45 +0530

    FLUT-3275 - Sample and screenshot added.

---
+++ android/app/src/main/kotlin/com/example/customfont_picker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.customfont_picker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
+
+void main() => runApp(CustomFont_Picker());
+
+class CustomFont_Picker extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Card(
+            margin: const EdgeInsets.fromLTRB(50, 150, 50, 150),
+            child: SfDateRangePicker(
+              view: DateRangePickerView.month,
+              headerStyle: DateRangePickerHeaderStyle(
+                  textStyle: TextStyle(
+                      fontFamily: 'SyneTactile', color: Colors.black)),
+              monthViewSettings: DateRangePickerMonthViewSettings(
+                viewHeaderStyle: DateRangePickerViewHeaderStyle(
+                    textStyle: TextStyle(
+                        fontFamily: 'SyneTactile', color: Colors.black)),
+              ),
+              monthCellStyle: DateRangePickerMonthCellStyle(
+                  textStyle:
+                      TextStyle(fontFamily: 'SyneTactile', color: Colors.black),
+                  todayTextStyle: TextStyle(
+                      fontFamily: 'SyneTactile', color: Colors.black)),
+              yearCellStyle: DateRangePickerYearCellStyle(
+                textStyle:
+                    TextStyle(fontFamily: 'SyneTactile', color: Colors.black),
+                disabledDatesTextStyle: const TextStyle(
+                    fontFamily: 'SyneTactile', color: Colors.black),
+                todayTextStyle:
+                    TextStyle(fontFamily: 'SyneTactile', color: Colors.black),
+                leadingDatesTextStyle:
+                    TextStyle(fontFamily: 'SyneTactile', color: Colors.black),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
